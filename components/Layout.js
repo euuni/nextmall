7f2b5becee7fbb8b1c0241f0d461f08a25b2958b
euuni <choidy0106@gmail.com>
2022-10-18 12:51:33 +0900

    first commit

---
+++ components/Layout.js
@@ -0,0 +1,44 @@
+import { useContext } from "react"
+import { Store } from "../utils/Store"
+import Head from "next/head"
+import Link from "next/link"
+
+export default function Layout({ title, children }) {
+  const { state } = useContext(Store)
+  const { cart } = state
+  return (
+    <>
+      <Head>
+        <title>{title ? title + " - Eunsinsa" : "Eunsinsa"}</title>
+        <meta name="description" content="Eunsinsa" />
+        <link rel="icon" href="favicon.ico" />
+      </Head>
+      <div className="flex min-h-screen flex-col justify-between">
+        <nav className="flex h-12 items-center px-4 justify-between shadow-md bg-slate-200">
+          <Link href="/">
+            <a className="text-lg font-bold">eunsinsa</a>
+          </Link>
+          <div>
+            <Link href="/cart">
+              <a className="p-2">
+                Cart
+                {cart.cartItems.length > 0 && (
+                  <span className="ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white ">
+                    {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                  </span>
+                )}
+              </a>
+            </Link>
+            <Link href="login">
+              <a className="p-2">Login</a>
+            </Link>
+          </div>
+        </nav>
+        <main className="container m-auto mt-4 px-4">{children}</main>
+        <footer className="flex h-10 justify-center items-center shadow-inner bg-red-100">
+          <p>Copyright &copy; 2022 Eunsinsa</p>
+        </footer>
+      </div>
+    </>
+  )
+}
